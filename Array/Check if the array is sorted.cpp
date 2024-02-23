62e68c228323d62368bb94637cf985ce9dacc704
Ritam Singha <128461736+Ritam-Singha@users.noreply.github.com>
2024-02-23 20:11:01 +0530

    Update Check if the array is sorted

--- Array/Check if the array is sorted.cpp
+++ Array/Check if the array is sorted.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main() {
-    int arr[] = {1, 2, 2, 4, 7, 5};
+    int arr[] = {1, 2, 2,4, 5,7};
     int n = sizeof(arr) / sizeof(arr[0]); 
     
-    // Check if array is sorted
-    bool isSorted = true;  // Assume array is sorted
+    
+    bool isSorted = true; 
     for (int i = 0; i < n - 1; i++) {
-        if (arr[i + 1] < arr[i]) {  // If any element is greater than the next one, array is not sorted
+        if (arr[i + 1] < arr[i]) {  
             isSorted = false;
-            break;  // No need to continue checking once unsorted element is found
-        }
+            break; 
     }
     
     if (isSorted) {
-        cout << "Array is sorted." << endl;
+        cout << " The Array is sorted." << endl;
     } else {
-        cout << "Array is not sorted." << endl;
+        cout << "The Array is not sorted." << endl;
     }
 
-    return 0;  // Return 0 to indicate successful execution
+    return 0;  
 }
+
